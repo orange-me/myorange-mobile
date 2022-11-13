b0ab042519eac0094f9f71840c51d9467ac93ee3
Njoku Emmanuel <kalunjoku123@gmail.com>
2022-11-13 14:30:56 +0400

    chore: setup redux constants 🍊

--- src/redux/constants.ts
+++ src/redux/constants.ts
@@ -1,6 +1,6 @@
 // -- Constants --------------------------------------- //
 
-// -- Wallet ------------------------------ //
+// -- Wallet/Auth ------------------------------ //
 export const CREATE_WALLET_SUCCESS = 'wallets/CREATE_WALLET_SUCCESS';
 export const CREATE_WALLET_FAILURE = 'wallets/CREATE_WALLET_FAILURE';
 
@@ -21,3 +21,21 @@ export const BACK_UP_WALLET_FAILURE = 'wallets/BACK_UP_WALLET_FAILURE';
 
 export const RESTORE_WALLET_SUCCESS = 'wallets/RESTORE_WALLET_SUCCESS';
 export const RESTORE_WALLET_FAILURE = 'wallets/RESTORE_WALLET_FAILURE';
+
+// -- Transaction ------------------------------ //
+export const TRANSACTION_LOAD_SUCCESS = 'transaction/TRANSACTION_LOAD_SUCCESS';
+export const SEND_CRYPTO_SUCCESS = 'transaction/SEND_CRYPTO_SUCCESS';
+export const SEND_CRYPTO_FAILURE = 'transaction/SEND_CRYPTO_FAILURE';
+
+export const LOAD_BALANCE = 'balance/LOAD_BALANCE';
+
+// -- Tokens ----- //
+export const IMPORT_TOKEN_SUCCESS = 'token/IMPORT_TOKEN_SUCCESS';
+export const IMPORT_TOKEN_FAILURE = 'token/IMPORT_TOKEN_SUCCESS';
+export const UPDATE_TOKEN_LIST = 'token/UPDATE_TOKEN_LIST';
+export const GET_TOKEN_PRICE = 'token/GET_TOKEN_PRICE';
+
+// -- Profile ------------------------------ //
+export const SET_PROFILE_PIC = 'token/SET_PROFILE_PIC';
+export const SET_LOCAL_CURRENCY = 'token/SET_LOCAL_CURRENCY';
+export const SET_SWITCH_NETWORK_SUCCESS = 'token/SET_SWITCH_NETWORK_SUCESS';
